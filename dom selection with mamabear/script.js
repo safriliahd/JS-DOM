0559379bb0f562abb608d9a0c84bc03d8009a31c
safriliahd <safrilianurhidayah@gmail.com>
2023-09-14 06:29:03 +0700

    tambah file

---
+++ dom selection with mamabear/script.js
@@ -0,0 +1,45 @@
+// const judul = document.getElementById('judul')
+// judul.style.color = 'red';
+// judul.style.backgroundColor = 'yellow';
+// judul.innerHTML = 'Hidaa';
+
+// const p = document.getElementsByTagName('p');
+// // p[0].style.backgroundColor = 'lightblue';
+// p[1].style.backgroundColor = 'grey';
+// p[2].style.backgroundColor = 'lightyellow';
+// p[3].style.backgroundColor = 'lightgreen';
+
+// for(let i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = 'lightgreen';
+// }
+
+// const h1 = document.getElementsByTagName('h1');
+// h1[0].style.fontSize = '50px';
+
+// const h1 = document.getElementsByTagName('h1')[0];
+// h1.style.fontSize = '50px';
+
+// const p1 = document.getElementsByClassName('p1');
+// p1[0].innerHTML = "Dirubah dengan Javascript :)"
+
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+// p4.style.fontSize = '25px';
+
+// const li2 = document.querySelector('section#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'lightcoral';
+
+// const p1 = document.querySelector('p');
+// p1.innerHTML = 'Ini dirubah melalui Javascript :)';
+
+// const p = document.querySelectorAll('p');
+
+// const p = document.getElementsByTagName('p');
+
+// const p = document.querySelectorAll('p');
+// p[2].style.backgroundColor = 'lightblue';
+
+const p = document.querySelectorAll('p');
+    for(let i = 0; i < p.length; i++){
+        p[i].style.backgroundColor = 'lightgreen';
+    }
